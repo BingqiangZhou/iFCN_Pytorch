943ef6f9aa0ce00ccda7708ffceb271ba1250294
BingqiangZhou <1299050656@qq.com>
2021-08-04 14:29:07 +0800

    iFCN

--- networks/FCN.py
+++ networks/FCN.py
@@ -7,28 +7,31 @@ class FCN(nn.Module):
         reference paper:
             Fully Convolutional Networks for Semantic Segmentation, https://arxiv.org/pdf/1411.4038.pdf
     '''
-    def __init__(self, backbone_name, num_classes=21) -> None:
+    def __init__(self, backbone_name, num_classes=21, stride_out=8, upsample_type='deconv') -> None:
         super(FCN, self).__init__()
         assert backbone_name in ['AlexNet', 'VGG', 
                             'ResNet18', 'ResNet34', 'ResNet50', 'ResNet101', 'ResNet152', 'SqueezeNet']
+        assert stride_out in [8, 16, 32] # fcn_8s, fcn_16s, fcn_32s
+        assert upsample_type in ['deconv', 'interpolate']
         
         if 'ResNet' in backbone_name:
             self.backbone_net = import_module('classifiers.ResNet', 'networks').NetRemoveFCLayer(backbone_name.lower())
         else:
             self.backbone_net = import_module(f'classifiers.{backbone_name}', 'networks').NetRemoveFCLayer()
         
-        self.classifier = FCNClassifier(self.backbone_net.out_channels, num_classes, stride_out=8)
+        self.classifier = FCNClassifier(self.backbone_net.out_channels, num_classes, stride_out=8, upsample_type=upsample_type)
 
     def forward(self, x):
         x_s32, x_s16, x_s8 = self.backbone_net(x)
-        print(x_s32.shape, x_s16.shape, x_s8.shape)
+        # print(x_s32.shape, x_s16.shape, x_s8.shape)
         out = self.classifier(x_s32, x_s16, x_s8)
         return out
 
 class FCNClassifier(nn.Module):
-    def __init__(self, in_channels, num_classes=21, stride_out=32):
+    def __init__(self, in_channels, num_classes=21, stride_out=32, upsample_type='deconv'):
         super(FCNClassifier, self).__init__()
         assert stride_out in [8, 16, 32] # fcn_8s, fcn_16s, fcn_32s
+        assert upsample_type in ['deconv', 'interpolate']
         
         if stride_out == 32:
             self.classifier = FCNClassifier32s(in_channels, num_classes)
@@ -42,11 +45,10 @@ class FCNClassifier(nn.Module):
         return out
 
 class FCNClassifier32s(nn.Module):
-    def __init__(self, in_channels, num_classes=21):
+    def __init__(self, in_channels, num_classes=21, upsample_type='deconv'):
         super(FCNClassifier32s, self).__init__()
-
-        self.upsample_s32 = nn.ConvTranspose2d(in_channels[0], num_classes, 32, 32, bias=False)
-
+        assert upsample_type in ['deconv', 'interpolate']
+        self.upsample_s32 = upsample_layer(32, upsample_type=upsample_type, in_channels=in_channels[0], out_channels=num_classes)
         self.classifier = nn.Conv2d(num_classes, num_classes, 1)
     
     def forward(self, x_s32, x_s16, x_s8):
@@ -55,13 +57,13 @@ class FCNClassifier32s(nn.Module):
         return out
 
 class FCNClassifier16s(nn.Module):
-    def __init__(self, in_channels, num_classes=21):
+    def __init__(self, in_channels, num_classes=21, upsample_type='deconv'):
         super(FCNClassifier16s, self).__init__()
-
-        self.upsample_s32 = nn.ConvTranspose2d(in_channels[0], num_classes, 2, 2, bias=False)
+        assert upsample_type in ['deconv', 'interpolate']
+        self.upsample_s32 = upsample_layer(2, upsample_type=upsample_type, in_channels=in_channels[0], out_channels=num_classes)
         self.conv_s16 = nn.Conv2d(in_channels[1], num_classes, 1, 1, bias=False)
         
-        self.upsample_s16 = nn.ConvTranspose2d(num_classes, num_classes, 16, 16, bias=False)
+        self.upsample_s16 = upsample_layer(16, upsample_type=upsample_type, in_channels=num_classes, out_channels=num_classes)
         self.classifier = nn.Conv2d(num_classes, num_classes, 1)
 
     def forward(self, x_s32, x_s16, x_s8):
@@ -77,18 +79,17 @@ class FCNClassifier16s(nn.Module):
         return out
 
 class FCNClassifier8s(nn.Module):
-    def __init__(self, in_channels, num_classes=21):
+    def __init__(self, in_channels, num_classes=21, upsample_type='deconv'):
         super(FCNClassifier8s, self).__init__()
+        assert upsample_type in ['deconv', 'interpolate']
 
-        self.upsample_s32 = nn.ConvTranspose2d(in_channels[0], num_classes, 2, 2, bias=False)
+        self.upsample_s32 = upsample_layer(2, upsample_type=upsample_type, in_channels=in_channels[0], out_channels=num_classes)
         
         self.conv_s16 = nn.Conv2d(in_channels[1], num_classes, 1, 1, bias=False)
-        self.upsample_s16 = nn.ConvTranspose2d(num_classes, num_classes, 2, 2, bias=False)
+        self.upsample_s16 = upsample_layer(2, upsample_type=upsample_type, in_channels=num_classes, out_channels=num_classes)
         
-        self.upsample_s8 = nn.ConvTranspose2d(num_classes, num_classes, 2, 2, bias=False)
         self.conv_s8 = nn.Conv2d(in_channels[2], num_classes, 1, 1, bias=False)
-        
-        self.upsample_s8 = nn.ConvTranspose2d(num_classes, num_classes, 8, 8, bias=False)
+        self.upsample_s8 = upsample_layer(8, upsample_type=upsample_type, in_channels=num_classes, out_channels=num_classes)
         self.classifier = nn.Conv2d(num_classes, num_classes, 1)
 
     def forward(self, x_s32, x_s16, x_s8):
@@ -109,3 +110,14 @@ class FCNClassifier8s(nn.Module):
         out = self.upsample_s8(out_s8_1 + out_s8_2)
         out = self.classifier(out)
         return out
+
+
+def upsample_layer(scale_factor, upsample_type='deconv', in_channels=0, out_channels=0):
+    if upsample_type == 'deconv':
+        upsample = nn.ConvTranspose2d(in_channels, out_channels, scale_factor, scale_factor, bias=False)
+    else:
+        upsample = nn.Sequential(
+            nn.Upsample(scale_factor=scale_factor),
+            nn.Conv2d(in_channels, out_channels, 1)
+        )
+    return upsample
--- networks/iFCN.py
+++ networks/iFCN.py
@@ -9,12 +9,14 @@ class iFCN(nn.Module):
         reference paper:
             Deep Interactive Object Selection, https://arxiv.org/abs/1603.04042.pdf
     '''
-    def __init__(self, backbone_name, in_channels=5, num_classes=21) -> None:
+    def __init__(self, backbone_name, in_channels=5, num_classes=1, stride_out=8, upsample_type='deconv', ) -> None:
         super(iFCN, self).__init__()
         assert backbone_name in ['AlexNet', 'VGG', 
                             'ResNet18', 'ResNet34', 'ResNet50', 'ResNet101', 'ResNet152', 'SqueezeNet']
+        assert stride_out in [8, 16, 32] # fcn_8s, fcn_16s, fcn_32s
+        assert upsample_type in ['deconv', 'interpolate']
         
-        self.fcn = FCN(backbone_name, num_classes)
+        self.fcn = FCN(backbone_name, num_classes, stride_out=stride_out, upsample_type=upsample_type)
 
         # modify first conv's in_channels
         first_conv = self.fcn.backbone_net.net.conv1
@@ -32,7 +34,7 @@ class iFCN(nn.Module):
 
 x = torch.rand(1, 5, 384, 384)
 # net = iFCN('ResNet18')
-net = iFCN('AlexNet')
+net = iFCN('AlexNet', stride_out=16, upsample_type='interpolate')
 
 out = net(x)
 print(out.shape)
